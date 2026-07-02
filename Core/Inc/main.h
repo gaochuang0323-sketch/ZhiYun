00d01fc0d05a93710923beb3b233bce795c9602c
hhx <gaochaung0323@163.com>
2026-07-02 10:25:03 +0800

    Initial commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,113 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define temp6a_Pin GPIO_PIN_6
+#define temp6a_GPIO_Port GPIOG
+#define temp6b_Pin GPIO_PIN_7
+#define temp6b_GPIO_Port GPIOG
+#define temp5b_Pin GPIO_PIN_6
+#define temp5b_GPIO_Port GPIOC
+#define temp5a_Pin GPIO_PIN_8
+#define temp5a_GPIO_Port GPIOC
+#define temp4b_Pin GPIO_PIN_9
+#define temp4b_GPIO_Port GPIOC
+#define temp4a_Pin GPIO_PIN_8
+#define temp4a_GPIO_Port GPIOA
+#define DAC_nCLR_Pin GPIO_PIN_2
+#define DAC_nCLR_GPIO_Port GPIOD
+#define DAC_RESET_Pin GPIO_PIN_3
+#define DAC_RESET_GPIO_Port GPIOD
+#define DAC_nLDAC_Pin GPIO_PIN_6
+#define DAC_nLDAC_GPIO_Port GPIOD
+#define temp3a_Pin GPIO_PIN_9
+#define temp3a_GPIO_Port GPIOG
+#define temp3b_Pin GPIO_PIN_10
+#define temp3b_GPIO_Port GPIOG
+#define temp7a_Pin GPIO_PIN_11
+#define temp7a_GPIO_Port GPIOG
+#define temp7b_Pin GPIO_PIN_12
+#define temp7b_GPIO_Port GPIOG
+#define temp2a_Pin GPIO_PIN_13
+#define temp2a_GPIO_Port GPIOG
+#define temp2b_Pin GPIO_PIN_15
+#define temp2b_GPIO_Port GPIOG
+#define temp1a_Pin GPIO_PIN_3
+#define temp1a_GPIO_Port GPIOB
+#define temp1b_Pin GPIO_PIN_4
+#define temp1b_GPIO_Port GPIOB
+#define TOGGLE2_Pin GPIO_PIN_5
+#define TOGGLE2_GPIO_Port GPIOB
+#define TOGGLE1_Pin GPIO_PIN_6
+#define TOGGLE1_GPIO_Port GPIOB
+#define TOGGLE0_Pin GPIO_PIN_7
+#define TOGGLE0_GPIO_Port GPIOB
+#define LED_YL_Pin GPIO_PIN_0
+#define LED_YL_GPIO_Port GPIOE
+#define LED_RY_Pin GPIO_PIN_1
+#define LED_RY_GPIO_Port GPIOE
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ Core/Src/gpio.c
@@ -0,0 +1,124 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins
+     PC14-OSC32_IN (OSC32_IN)   ------> RCC_OSC32_IN
+     PC15-OSC32_OUT (OSC32_OUT)   ------> RCC_OSC32_OUT
+     PH0-OSC_IN (PH0)   ------> RCC_OSC_IN
+     PH1-OSC_OUT (PH1)   ------> RCC_OSC_OUT
+     PA13 (JTMS/SWDIO)   ------> DEBUG_JTMS-SWDIO
+     PA14 (JTCK/SWCLK)   ------> DEBUG_JTCK-SWCLK
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, DAC_nCLR_Pin|DAC_RESET_Pin|DAC_nLDAC_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, TOGGLE2_Pin|TOGGLE1_Pin|TOGGLE0_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, LED_YL_Pin|LED_RY_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : temp6a_Pin temp6b_Pin temp3a_Pin temp3b_Pin
+                           temp7a_Pin temp7b_Pin temp2a_Pin temp2b_Pin */
+  GPIO_InitStruct.Pin = temp6a_Pin|temp6b_Pin|temp3a_Pin|temp3b_Pin
+                          |temp7a_Pin|temp7b_Pin|temp2a_Pin|temp2b_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : temp5b_Pin temp5a_Pin temp4b_Pin */
+  GPIO_InitStruct.Pin = temp5b_Pin|temp5a_Pin|temp4b_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : temp4a_Pin */
+  GPIO_InitStruct.Pin = temp4a_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(temp4a_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : DAC_nCLR_Pin DAC_RESET_Pin DAC_nLDAC_Pin */
+  GPIO_InitStruct.Pin = DAC_nCLR_Pin|DAC_RESET_Pin|DAC_nLDAC_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : temp1a_Pin temp1b_Pin */
+  GPIO_InitStruct.Pin = temp1a_Pin|temp1b_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : TOGGLE2_Pin TOGGLE1_Pin TOGGLE0_Pin */
+  GPIO_InitStruct.Pin = TOGGLE2_Pin|TOGGLE1_Pin|TOGGLE0_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LED_YL_Pin LED_RY_Pin */
+  GPIO_InitStruct.Pin = LED_YL_Pin|LED_RY_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /**/
+  HAL_I2CEx_EnableFastModePlus(SYSCFG_PMCR_I2C_PB6_FMP);
+
+  /**/
+  HAL_I2CEx_EnableFastModePlus(SYSCFG_PMCR_I2C_PB7_FMP);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
